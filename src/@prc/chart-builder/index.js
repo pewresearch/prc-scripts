5254319cb0036c5d5aba5941d62981d33c831922
Ben Wormald <benwormald@gmail.com>
2022-12-06 13:59:50 -0500

    #3283 abstract dependencies

--- src/@prc/chart-builder/index.js
+++ src/@prc/chart-builder/index.js
@@ -1,6 +1,7 @@
 import {
 	ChartBuilderWrapper,
 	masterConfig,
+	ChartBuilderTextWrapper,
 } from '@pewresearch/chart-builder/dist';
 
 function loadScript(slug, script) {
@@ -12,6 +13,7 @@ function loadScript(slug, script) {
 window.prcChartBuilder = {};
 
 loadScript('ChartBuilderWrapper', ChartBuilderWrapper);
+loadScript('ChartBuilderTextWrapper', ChartBuilderTextWrapper);
 loadScript('masterConfig', masterConfig);
 
 console.log('Loading @prc/chart-builder...', window.prcChartBuilder);
