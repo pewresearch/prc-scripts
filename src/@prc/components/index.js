4cccaae1774a4b6eb7e3ae11dfdc701d97b81d58
Seth Rubenstein <srubenstein@pewresearch.org>
2023-01-02 15:45:08 -0500

    Basics of taxonomy search are scaffolded out Working on new term select component

--- src/@prc/components/index.js
+++ src/@prc/components/index.js
@@ -8,6 +8,7 @@ import {
 } from './list-store';
 import MediaDropZone from './media-dropzone';
 import TaxonomySelect from './taxonomy-select';
+import TermSelect from './term-select';
 
 function loadScript(slug, script) {
 	if (!window.prcComponents[slug]) {
@@ -26,5 +27,6 @@ loadScript('listStoreActions', actions);
 loadScript('listStoreReducer', reducer);
 loadScript('MediaDropZone', MediaDropZone);
 loadScript('TaxonomySelect', TaxonomySelect);
+loadScript('TermSelect', TermSelect);
 
 console.log('Loading @prc/components...', window.prcComponents);
---
+++ src/@prc/components/term-select/index.jsx
@@ -0,0 +1,83 @@
+/**
+ * External Dependencies
+ */
+import { MultiSelectControl } from '@codeamp/block-components';
+import styled from '@emotion/styled';
+
+/**
+ * WordPress Dependencies
+ */
+import { SelectControl, Spinner } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { useEffect, useState } from '@wordpress/element';
+
+const MultiSelectWrapper = styled('div')`
+	& .components-button.has-icon {
+		padding: 0px !important;
+	}
+`;
+
+export default function TermSelect({
+	className,
+	value,
+	onChange,
+	allowMultiple = false,
+}) {
+	const [currentValue, setCurrentValue] = useState(value);
+	const { records } = useSelect((select) => {
+		const { getEntitiesConfig } = select('core');
+		return {
+			records: getEntitiesConfig('taxonomy'),
+		};
+	});
+	const [tokens, setTokens] = useState([]);
+
+	useEffect(() => {
+		if (0 < records.length && 0 === tokens.length) {
+			const newTokens = records.map((taxonomy) => ({
+				label: taxonomy.label,
+				value: taxonomy.name,
+			}));
+			setTokens(newTokens);
+		}
+	}, [records]);
+
+	useEffect(() => {
+		if (currentValue) {
+			console.log('Value Changed 2: ', currentValue);
+			onChange(currentValue);
+		}
+	}, [currentValue]);
+
+	const hasTokens = tokens ? 0 < tokens.length : false;
+
+	return (
+		<div className={className}>
+			{!hasTokens && <Spinner />}
+			{hasTokens && !allowMultiple && (
+				<SelectControl
+					label="Taxonomy"
+					value={currentValue}
+					options={tokens}
+					onChange={(newValue) => {
+						console.log('Value Changed 1: ', newValue);
+						setCurrentValue(newValue);
+					}}
+					__nextHasNoMarginBottom
+				/>
+			)}
+			{hasTokens && allowMultiple && (
+				<MultiSelectWrapper>
+					<MultiSelectControl
+						label="Taxonomy"
+						value={currentValue}
+						options={tokens}
+						onChange={(newValue) => {
+							setCurrentValue(newValue);
+						}}
+					/>
+				</MultiSelectWrapper>
+			)}
+		</div>
+	);
+}
