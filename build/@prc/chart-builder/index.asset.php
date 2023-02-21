46c01c4e87d90aa31aaeeae6d968c4422d00f720
benwormald <benwormald@gmail.com>
2023-02-21 14:13:00 -0500

    #3241 convert label controls to toolspanel

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'a9a4cf98da99bf45b977');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '9e268d3ea4da6cb84bfe');
