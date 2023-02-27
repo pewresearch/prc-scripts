53ee1d0c62be85ead9756f584787fc930a73e550
Ben Wormald <benwormald@gmail.com>
2023-02-27 12:51:41 -0500

    #3241 clean up label and tooltip controls

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '885bf3d871fdaeae8b50');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'e93dec941e2c05baaf42');
