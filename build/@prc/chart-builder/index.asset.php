40cd5e95da2130a2f2825e5f04a1d63e00593f78
Ben Wormald <benwormald@gmail.com>
2023-02-27 15:19:29 -0500

    #3241 divergent bar attributes added

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'e93dec941e2c05baaf42');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '11c7be2cdb8027dfc370');
