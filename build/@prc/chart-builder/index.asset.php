0d7dbd65f9eeb9173b03c0795a52ea90a6e275d7
Ben Wormald <benwormald@gmail.com>
2023-03-02 13:36:28 -0500

    #3241 mostly working updated progress bar

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'c4c3d8f4e94d7f19a0fb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'e9aef908ef9c628d6a8e');
