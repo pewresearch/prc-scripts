66914313115f4cb937ed8c26658ae38d08ace9ec
Ben Wormald <benwormald@gmail.com>
2023-03-06 11:54:36 -0500

    #3241 tweak some RLS config. remove some logs

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'd6ddbec4bf1f1c667772');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '191925934deeb79edbe7');
