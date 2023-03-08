17092829ff8f7cbf9c0ed13bfdff28bb61eefffa
Ben Wormald <benwormald@gmail.com>
2023-03-08 10:09:07 -0500

    #3241 resolve column chart rendering

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '191925934deeb79edbe7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '63d921b3e5858bec86a4');
