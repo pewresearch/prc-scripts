a70046db35b2b71498d90d318aae8065c4c20eae
Ben Wormald <benwormald@gmail.com>
2023-03-16 11:19:09 -0400

    #3241 fix label rendering and ticks for histo

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '63d921b3e5858bec86a4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '3a479bd0af6c19675790');
