ad08d48eaa3f187a27cee5bedeee1d736ed9fe22
Ben Wormald <benwormald@gmail.com>
2023-03-27 16:16:27 -0400

    #3241 update RLS tooltips

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '25b56fb0156273eaae8d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '73eb87175665b3c0d559');
