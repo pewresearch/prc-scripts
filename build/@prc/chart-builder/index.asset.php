150ed4e166c58a4ff78a0b7f13d1eb135244850d
Ben Wormald <benwormald@gmail.com>
2023-03-27 13:36:39 -0400

    #3241 rebuild cb script

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'c3dd3a1d280af2226577');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '25b56fb0156273eaae8d');
