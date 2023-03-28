1a118304bf5d0aa09fa23eed810b518009ec036a
benwormald <benwormald@gmail.com>
2023-03-28 12:34:57 -0400

    #3241 numerous style edits pt 2

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '73eb87175665b3c0d559');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '01a0a32750a0a1e1d197');
