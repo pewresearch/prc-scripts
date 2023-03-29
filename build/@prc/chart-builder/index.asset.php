1f925368d270cb69b27f74eae5412d9b3ded24f2
Ben Wormald <benwormald@gmail.com>
2023-03-29 11:07:59 -0400

    #3241 fix divergent axis mobile render

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '572f61fec2bb8da2d9d9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '7a65875e92848e0b126e');
