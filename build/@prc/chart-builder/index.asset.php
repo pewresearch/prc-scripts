b0bb4992b4151baddee8051bb3a18b95088d8a8a
Ben Wormald <benwormald@gmail.com>
2023-03-31 17:08:31 -0400

    #cb-hotfix fix sort issue on line charts

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '929e3d8bc5b369aaa6cc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'e2c97c7dfb2f33cc3cbf');
