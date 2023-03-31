8aee2b52d6d194363ba466c37399d7bc917a1258
Ben Wormald <benwormald@gmail.com>
2023-03-31 15:51:46 -0400

    #4.0.12-cb-labelfix bump cb package, expose label positioning

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '7a65875e92848e0b126e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '929e3d8bc5b369aaa6cc');
