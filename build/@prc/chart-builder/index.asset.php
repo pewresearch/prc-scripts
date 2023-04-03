bf854b6d4deebe038779a6c29c8c2a94fdee1099
Ben Wormald <benwormald@gmail.com>
2023-04-03 12:02:50 -0400

    #3439 add number formatting for labels and tooltips

--- build/@prc/chart-builder/index.asset.php
+++ build/@prc/chart-builder/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'e2c97c7dfb2f33cc3cbf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '7cb1b8c4b61e65d12c3a');
