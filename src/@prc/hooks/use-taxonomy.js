aff96780100d4ea96528ccfd1762365940ded6a7
prcdevgitbot <webdev@pewresearch.org>
2024-05-07 22:08:11 +0000

    See https://github.com/pewresearch/pewresearch-org/commit/806f36c7cbe026af6d6d4ab548563424519889e7 from refs/heads/main

--- src/@prc/hooks/use-taxonomy.js
+++ src/@prc/hooks/use-taxonomy.js
@@ -5,25 +5,29 @@ import { __ } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
 
-export default function useTaxonomy(taxonomy = null, slug =  null, fields = [ 'id', 'name' ]) {
-	const { taxId, taxName } = useSelect( select => {
-		const { getEntityRecords } = select( coreStore );
+export default function useTaxonomy(
+	taxonomy = null,
+	slug = null,
+	fields = ['id', 'name']
+) {
+	const { taxId, taxName } = useSelect((select) => {
+		const { getEntityRecords } = select(coreStore);
 
-		const tax = null !== slug ? getEntityRecords( 'taxonomy', taxonomy, {
-			context: 'view',
-			per_page: 1,
-			_fields: fields,
-			slug: slug,
-		} ) : null;
+		const tax =
+			null !== slug
+				? getEntityRecords('taxonomy', taxonomy, {
+						context: 'view',
+						per_page: 1,
+						_fields: fields,
+						slug,
+					})
+				: null;
 
 		return {
-			taxId: tax?.[ 0 ]?.id,
-			taxName: tax?.[ 0 ]?.name,
-		}
+			taxId: tax?.[0]?.id,
+			taxName: tax?.[0]?.name,
+		};
 	});
 
-	return [
-		taxId,
-		taxName,
-	];
+	return [taxId, taxName];
 }
