2e14c6f4d2d5a5204e6c7db5de35cbae90d57180
prcdevgitbot <webdev@pewresearch.org>
2026-07-27 15:54:10 +0000

    See https://github.com/pewresearch/prc-platform/commit/e11ddc6a6f965dc2fcc3bdf577744f175d99eee6 from refs/tags/v1.12.3

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'pewresearch/prc-scripts',
         'pretty_version' => 'dev-trunk',
         'version' => 'dev-trunk',
-        'reference' => '57477e5cf4abd36f4cb9b6345434f268ca923b35',
+        'reference' => '60e0f63163ba422e2200566d7b7ee190c48d0453',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'pewresearch/prc-scripts' => array(
             'pretty_version' => 'dev-trunk',
             'version' => 'dev-trunk',
-            'reference' => '57477e5cf4abd36f4cb9b6345434f268ca923b35',
+            'reference' => '60e0f63163ba422e2200566d7b7ee190c48d0453',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
